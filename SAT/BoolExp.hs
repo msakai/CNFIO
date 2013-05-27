38a7c377b986ff4af0d716ab5ee0f2a06df05a21
Shuji Narazaki <shuji.narazaki@gmail.com>
2013-05-28 00:17:19 +0900

    reimplement SAT.BooExp.hs in order to use strings as literals; version 0.1.4.1

--- SAT/BoolExp.hs
+++ SAT/BoolExp.hs
@@ -1,47 +1,28 @@
-{-# LANGUAGE GADTs, EmptyDataDecls  #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
 -- | Boolean Expression module to build CNF from arbitrary expressions
 --
 -- TODO: 
 --
 -- * redefine as a "Functor"
 --
-module SAT.BoolExp where
+module SAT.BoolExp 
+       ( 
+         -- * Expression contructors
+         (-|-)
+       , (-&-)
+       , neg
+         -- * Convert function
+       , asList
+       )
+       where
 
--- | BoolForm型
-data BoolForm =
-  Lit Int                       -- ^ a literal in BoolForm
-  | Cls [BoolForm]              -- ^ a clause in BoolForm
-  | Cnf [BoolForm]              -- ^ well-structured BoolForm
-    deriving (Eq, Show)
-      
--- | negate "BoolForm"
--- 
--- >>> let (l1, l2, l3, l4) = (Lit 1, Lit 2, Lit 3, Lit 4)
--- >>> let (c1, c2, c3) = (l1 -|- l2, l2 -|- l3, l3 -|- l4 )
--- >>> asList $ neg l1
--- [[-1]]
---
--- >>> asList $ neg c1
--- [[-1],[-2]]
---
--- >>> let cnf = c1 -&- (c2 -&- c3)
--- >>> asList cnf
--- [[1,2],[2,3],[3,4]]
---
--- >>> asList $ neg cnf
--- [[-1,-2,-3],[-1,-2,-4],[-1,-3,-3],[-1,-3,-4],[-2,-2,-3],[-2,-2,-4],[-2,-3,-3],[-2,-3,-4]]
---
-neg :: BoolForm -> BoolForm
-neg (Lit n) = Lit $ negate n
-neg (Cls ls) = Cnf [Cls [(neg l)] | l <- ls]
-neg cnf@(Cnf _) = Cnf [Cls (map (Lit . negate) l) | l <- combinationOf lits]
-  where
-    lits = asList cnf
+class BoolComponent a where
+  -- | lift to BoolForm
+  toBF :: a -> BoolForm
 
--- | Disjunction constructor
+-- | disjunction constructor
 --
--- >>> let c1 = (Lit 3 -|- Lit 4)
+-- >>> let c1 = "3" -|- "4"
 -- >>> c1
 -- Cls [Lit 3,Lit 4]
 --
@@ -51,33 +32,71 @@ neg cnf@(Cnf _) = Cnf [Cls (map (Lit . negate) l) | l <- combinationOf lits]
 -- >>> asList (c1 -|- Lit (-1))
 -- [[3,4,-1]]
 --  
-(-|-) l1@(Lit a) l2@(Lit b) = Cls [l1, l2]
-(-|-) (Cls ls) lit@(Lit l) = Cls $ ls ++ [lit]
-(-|-) (Cls as) (Cls bs) = Cls $ as ++ bs
-(-|-) (Cnf cnf) l@(Lit _) = Cnf [ c -|- l | c <- cnf]
-(-|-) (Cnf cnf) c@(Cls _) = Cnf [ c' -|- c | c' <- cnf]
-(-|-) cnf@(Cnf _) (Cnf cs) = foldr (\c cnf -> cnf -|- c) cnf cs
-(-|-) a b = b -|- a
+(-|-) :: (BoolComponent a, BoolComponent b) => a -> b -> BoolForm
+a -|- b = toBF a -||- toBF b
 
--- | Conjunction constructor
+-- | conjunction constructor
 --
--- >>> let (l1, l2, l3, l4) = (Lit 1, Lit 2, Lit 3, Lit 4)
--- >>> let (n1, n2, n3, n4) = (Lit (-1), Lit (-2), Lit (-3), Lit (-4))
--- >>> asList $ l3 -&- n2
+-- >>> asList $ "3" -&- "-2"
 -- [[3],[-2]]
 --
--- >>> asList $ l3 -|- (l1 -&- l2)
+-- >>> asList $ "3" -|- ("1" -&- "2")
 -- [[1,3],[2,3]]
 --
-(-&-) :: BoolForm -> BoolForm -> BoolForm
-(-&-) a@(Lit _) b@(Lit _) = Cnf [Cls [a], Cls [b]]
-(-&-) c@(Cls _) l@(Lit _) = Cnf [c, Cls [l]]
-(-&-) a@(Cls _) b@(Cls _) = Cnf [a, b]
-(-&-) (Cnf cnf) l@(Lit _) = Cnf $ Cls [l]: cnf
-(-&-) (Cnf cnf) c@(Cls _) = Cnf $ c : cnf
-(-&-) a b = b -&- a
+(-&-) :: (BoolComponent a, BoolComponent b) => a -> b -> BoolForm
+a -&- b = toBF a -&&- toBF b
 
--- | converts "CNF" to "[[Int]]"
+-- | negate a form
+-- 
+-- >>> let (c1, c2, c3) = ("1" -|- "2", "2" -|- "3", "3" -|- "4" )
+-- >>> asList $ neg c1
+-- [[-1],[-2]]
+--
+-- >>> let cnf = c1 -&&- (c2 -&&- c3)
+-- >>> asList cnf
+-- [[1,2],[2,3],[3,4]]
+--
+-- >>> asList $ neg cnf
+-- [[-1,-2,-3],[-1,-2,-4],[-1,-3,-3],[-1,-3,-4],[-2,-2,-3],[-2,-2,-4],[-2,-3,-3],[-2,-3,-4]]
+--
+neg :: (BoolComponent a) => a -> BoolForm
+neg a = negBF $ toBF a
+
+instance BoolComponent [Char] where
+  toBF a = Lit (read a::Int)
+
+-- | BoolForm型
+data BoolForm =
+  Lit Int                       -- ^ a literal in BoolForm
+  | Cls [BoolForm]              -- ^ a clause in BoolForm
+  | Cnf [BoolForm]              -- ^ well-structured BoolForm
+    deriving (Eq, Show)
+      
+instance BoolComponent BoolForm where
+  toBF = id
+
+(-||-) l1@(Lit a) l2@(Lit b) = Cls [l1, l2]
+(-||-) (Cls ls) lit@(Lit l) = Cls $ ls ++ [lit]
+(-||-) (Cls as) (Cls bs) = Cls $ as ++ bs
+(-||-) (Cnf cnf) l@(Lit _) = Cnf [ c -||- l | c <- cnf]
+(-||-) (Cnf cnf) c@(Cls _) = Cnf [ c' -||- c | c' <- cnf]
+(-||-) cnf@(Cnf _) (Cnf cs) = foldr (\c cnf -> cnf -||- c) cnf cs
+(-||-) a b = b -||- a
+
+(-&&-) a@(Lit _) b@(Lit _) = Cnf [Cls [a], Cls [b]]
+(-&&-) c@(Cls _) l@(Lit _) = Cnf [c, Cls [l]]
+(-&&-) a@(Cls _) b@(Cls _) = Cnf [a, b]
+(-&&-) (Cnf cnf) l@(Lit _) = Cnf $ Cls [l]: cnf
+(-&&-) (Cnf cnf) c@(Cls _) = Cnf $ c : cnf
+(-&&-) a b = b -&&- a
+
+negBF (Lit n) = Lit $ negate n
+negBF (Cls ls) = Cnf [Cls [(negBF l)] | l <- ls]
+negBF cnf@(Cnf _) = Cnf [Cls (map (Lit . negate) l) | l <- combinationOf lits]
+  where
+    lits = asList cnf
+
+-- | converts a BoolForm to "[[Int]]"
 asList :: BoolForm -> [[Int]]
 asList a@(Lit i) = [[i]]
 asList c@(Cls _) = [asClauseList c]
